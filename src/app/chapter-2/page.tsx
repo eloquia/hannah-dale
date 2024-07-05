97bdfd7c0d2ea8ec8d1f472f1c7f552b9ab8b205
Dale Chang <dalecb13@gmail.com>
2024-07-05 09:45:47 -0700

    added ability to check guess

--- src/app/chapter-2/page.tsx
+++ src/app/chapter-2/page.tsx
@@ -6,14 +6,15 @@ import { useRouter } from 'next/navigation';
 import Map from '@/components/map/map';
 import Link from 'next/link';
 import Modal from '../image/page';
+import getDistance from 'geolib/es/getDistance';
 
 const data = [
   {
     id: 0,
     imageSrc: '/chapter-2/germany.jpg',
-    imageAlt: 'germany',
-    x: 0,
-    y: 0,
+    imageAlt: 'Brandenburg Gate',
+    lng: 13.377775,
+    lat: 52.516266,
   },
 ];
 
@@ -26,8 +27,10 @@ export default function Chapter2({ searchParams }: SearchParamProps) {
   const [imageSrc, setImageSrc] = useState('');
   const [imageAlt, setImageAlt] = useState('');
   const router = useRouter();
-  const [lng, setLng] = useState(-117.120830);
-  const [lat, setLat] = useState(32.760840);
+  const [initialLng, setLng] = useState(-117.120830);
+  const [initialLat, setLat] = useState(32.760840);
+  const [guessLng, setGuessLng] = useState(-117.120830);
+  const [guessLat, setGuessLat] = useState(32.760840);
 
   useEffect(() => {
     setImageSrc('/chapter-2/germany.jpg');
@@ -39,20 +42,35 @@ export default function Chapter2({ searchParams }: SearchParamProps) {
   }
 
   const handleGetLngLat = (lng: number, lat: number) => {
-    console.log(lng, lat);
+    setGuessLng(lng);
+    setGuessLat(lat);
+  }
+
+  const handleGuess = () => {
+    const ACCURACY_IN_METERS = 1000;
+    const distanceInMeters = getDistance({ latitude: guessLat, longitude: guessLng }, { latitude: data[0].lat, longitude: data[0].lng }, ACCURACY_IN_METERS);
+
+    // compare guess lng and guess lat with data lng and lat
+    if (distanceInMeters === 0) {
+      // router.push('/chapter-2/win');
+      console.log('good guess!');
+    } else {
+      // router.push('/chapter-2/lose');
+      console.log('bad guess!');
+    }
   }
 
   return (
     <>
       <div className="">
-        <Map getLngLat={handleGetLngLat} lng={lng} lat={lat} />
+        <Map getLngLat={handleGetLngLat} lng={initialLng} lat={initialLat} />
         <div className="chapter-2-actions flex justify-between p-4">
           <Link href="/chapter-2?show=true">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
             </svg>
           </Link>
-          <button className="btn btn-primary" onClick={handleMapClick}>Submit</button>
+          <button className="btn btn-primary" onClick={handleGuess}>Guess</button>
         </div>
       </div>
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const HEX_TEAL = '#00d1b2';
 export const HEX_BRIGHT_TEAL = "#01F9C6";
+
+export const RADIUS_OF_ERROR = 0;
